cdb5fc42b28ea2bce224fa2f4e0b8f78583edd0a
Mesti-N <nicolasmesti13@gmail.com>
2025-11-14 11:31:35 -0500

    Test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js App! After Secrets set up!');
+    res.send('Hello, Dockerized Node.js App! Test');
 });
 
 app.listen(PORT, () => {
